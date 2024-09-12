a68ecebe762a6b849288c99735b3715d53799cfd
Karishma Panchbhai <pachbhaikarishma05@gmail.com>
2024-09-12 17:17:14 +0530

    Arrays Programs

---
+++ Double_char_Boolean.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class Double_char_Boolean
+{
+public static void main(String[] args) 
+{
+	double weight[]=new double[3];
+	weight[0]=15.2;
+	weight[1]=16.1;
+	weight[2]=14.1;
+	char Letter[]=new char[3];
+	Letter[0]='K';
+	Letter[1]='A';
+	Letter[2]='S';
+
+	for(int i=0;i<3;i++)
+	{
+	System.out.println("Letter is->"+Letter[i] +"  and weight is->" +weight[i]);
+	
+	}
+	
+	
+	
+	
+}
+}
